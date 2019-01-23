27a39f1dc3ccd7934c83fa3e57929da48f3bf8c9
Tim Übelhör <t.uebelhoer@irt.rwth-aachen.de>
2019-01-23 16:28:47 +0100

    Removed requirement for this package in the test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,9 +9,6 @@ class Gl3wTestConan(ConanFile):
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = "shared=True", "fPIC=True"
 
-    def requirements(self):
-        self.requires("gl3w/0.2@{}/{}".format(self.user, self.channel))
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
